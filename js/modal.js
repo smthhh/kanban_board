a2432960cc1c4a72124600f78d41d149971a51f7
Yana <yanahaletskaya2002@gmail.com>
2025-06-16 12:32:05 +0300

    add a hiding element for the opened modal window

--- js/modal.js
+++ js/modal.js
@@ -3,4 +3,16 @@ const modal = document.querySelector('.about-block');
 
 openBtn.addEventListener('click', () => {
     modal.classList.remove('hide');
-})
+});
+
+const closeBtn = modal.querySelector('.close-modal');
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.add('hide');
+    }
+});
